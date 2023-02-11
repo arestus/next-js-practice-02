ae05424ad312daeeda22081e5d77babca7c92176
arestus <arestus@gmail.com>
2023-02-11 18:23:44 +0100

    adds styles and change components from js to tsx

--- components/Footer.js
+++
@@ -1,9 +0,0 @@
-import Heading from "./Heading";
-
-const Footer = () => (
-  <footer>
-    <Heading tag="h3" text="Footer" />
-  </footer>
-);
-
-export default Footer;
--- components/Header.js
+++
@@ -1,9 +0,0 @@
-import Heading from "./Heading";
-
-const Header = () => (
-  <header>
-    <Heading tag="h3" text="Header" />
-  </header>
-);
-
-export default Header;
