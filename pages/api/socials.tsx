1cc03936a8761fcde455e382c596a5280616c187
arestus <arestus@gmail.com>
2023-02-13 14:29:57 +0100

    adds api for socials route

---
+++ pages/api/socials.tsx
@@ -0,0 +1,8 @@
+import { socials } from "./data/socials";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === "GET") {
+    res.status(200).json(socials);
+  }
+}
