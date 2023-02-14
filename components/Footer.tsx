b8d0415d5505d186f99c0b0c0733025b92f5487a
arestus <arestus@gmail.com>
2023-02-14 16:51:51 +0100

    adds Footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,10 @@
+import Heading from "./Heading";
+import { FC } from "react";
+
+const Footer: FC = () => (
+  <footer>
+    <Heading tag="h3" text="Footer" />
+  </footer>
+);
+
+export default Footer;
